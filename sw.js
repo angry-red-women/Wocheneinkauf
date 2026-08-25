c2c15c4cadb35ef78f846a8870036f976715246f
angry-red-women <marcel.nussbaumer@gmail.com>
2026-08-25 16:51:24 +0200

    Aktualisiere PWA Cache für Cloud-Synchronisierung

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wocheneinkauf-v2';
+const CACHE='wocheneinkauf-v3';
 const FILES=['./','index.html','style.css','app.js','manifest.json','icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
