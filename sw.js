fa0b71f232407363f4a22b565dac84ae1b340631
angry-red-women <marcel.nussbaumer@gmail.com>
2026-08-30 22:39:52 +0200

    PWA Cache für Mengenfunktion aktualisieren

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='wocheneinkauf-v3';
-const FILES=['./','index.html','style.css','app.js','manifest.json','icon.svg'];
+const CACHE='wocheneinkauf-v4';
+const FILES=['./','index.html','style.css','app.js','auth.js','manifest.json','icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
