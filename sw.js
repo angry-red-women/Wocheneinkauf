5aed5b1ac0d449a2d80b75820e7071b90d43d729
angry-red-women <marcel.nussbaumer@gmail.com>
2026-08-31 20:50:54 +0200

    Neue Rezeptdaten im PWA Cache aufnehmen

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='wocheneinkauf-v5';
-const FILES=['./','index.html','style.css','recipe-model.js','app.js','auth.js','manifest.json','icon.svg'];
+const CACHE='wocheneinkauf-v6';
+const FILES=['./','index.html','style.css','recipe-model.js','recipe-data.js','app.js','auth.js','manifest.json','icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
